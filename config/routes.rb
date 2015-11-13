c6d2818c35f7acf62e02903de76ae0a25de4bfed
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-13 03:16:16 +0000

    Handle form submission

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root 'welcome#index'
+  root 'users#my_portfolio'
   
   get 'my_portfolio',   to: 'users#my_portfolio'
   get 'search_stocks',  to: 'stocks#search'
